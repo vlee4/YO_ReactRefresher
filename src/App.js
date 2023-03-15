6657b70af05b7e4cd8473a53e1c32fe698830b80
vlee4 <vicglee1234007@gmail.com>
2023-03-14 23:23:25 -0500

    Integrate individual task components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import Header from './Components/Header'
+import Header from './Components/Header';
+import Tasks from './Components/Tasks';
+import React, { useState } from 'react';
 
 function App() {
+  const taskData = [
+    {
+        id: 1,
+        text: "Do Something",
+        day: 'Mar 14 at 2:34pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: "Do Something Else",
+        day: 'Mar 15 at 6:54pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: "Do Something Different",
+        day: 'Mar 16 at 5:43pm',
+        reminder: false,
+    },
+]
+//eslint-disable-next-line
+  const [tasks, setTasks] = useState(taskData)
 
   return (
     <div className="container">
-      <Header/>
+      <Header />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
