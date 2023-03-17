2d8d01ac6349540e78a2b92584239fc02623886f
vlee4 <vicglee1234007@gmail.com>
2023-03-16 20:53:28 -0500

    Add inputted task info to list

--- src/App.js
+++ src/App.js
@@ -36,10 +36,17 @@ function App() {
     setTasks(tasks.map(task => task.id===id ? {...task, reminder: !task.reminder}:task))
   }
 
+  //Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000 + 1)
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
   return (
     <div className="container">
       <Header />
-      <AddTask/>
+      <AddTask onAdd={addTask}/>
       {tasks.length>0? <Tasks onDelete={deleteTask} onToggle={toggleReminder} tasks={tasks}/>: "No Tasks to Show"}
     </div>
   );
